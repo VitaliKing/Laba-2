3a0aa8430adc1f666f5a80dc391d83f10dd7fae1
Vitalik <Vitalik.Trofimenko.1998@mail.ru>
2016-10-02 17:03:35 +0700

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите возраст:");
+            a = int.Parse(Console.ReadLine());
+
+            if ( a <= 1) 
+            {
+                Console.WriteLine("Младенец");
+            }
+            if (a > 1 && a < 11)
+            {
+             Console.WriteLine("Ребенок");
+            }
+            if (a > 12 && a < 15)
+            {
+                Console.WriteLine("Подросток");
+            }
+            if (a > 16 && a < 25)
+            {
+                Console.WriteLine("Юноша");
+            }
+            if (a > 26 && a < 70)
+            {
+                Console.WriteLine("Мужчина");
+            }
+            if (a > 70 )
+            {
+                Console.WriteLine("Старик");
+            }
+               
+
+            Console.ReadKey();
+        }
+    }
+}
