b04facbd4ed530dc7d62784b4ef4d05dac98a313
Vitalik <Vitalik.Trofimenko.1998@mail.ru>
2016-10-02 17:02:26 +0700

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 14: Console.WriteLine("Туз"); break;
+                case 13: Console.WriteLine("Король"); break;
+                case 12: Console.WriteLine("Дама"); break;
+                case 11: Console.WriteLine("Валет"); break;
+                case 10: Console.WriteLine("Десятка"); break;
+                case 9: Console.WriteLine("Девятка"); break;
+                case 8: Console.WriteLine("Восьмерка"); break;
+                case 7: Console.WriteLine("Семерка"); break;
+                case 6: Console.WriteLine("Шестерка"); break;
+                default: Console.WriteLine("Вы ошыблись!!!"); break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
